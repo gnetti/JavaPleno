c1a6c4b727c0e4e4987a0829dedd149d004d689b
Luiz Generoso <luizerytre@gmail.com>
2023-01-07 15:16:03 -0300

    Adicionado Chamada para api Colaborador pelo Feign.

---
+++ rh.pagamento/src/main/java/rh/javapleno/pagamento/component/ColaboradorFeignClient.java
@@ -0,0 +1,15 @@
+package rh.javapleno.pagamento.component;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import rh.javapleno.pagamento.model.Colaborador;
+
+@Component
+@FeignClient(name = "rh-colaborador", url = "localhost:8001", path = "/colaborador")
+public interface ColaboradorFeignClient {
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Colaborador> findById(@PathVariable Long id);
+}
--- rh.pagamento/src/main/java/rh/javapleno/pagamento/service/PagamentoService.java
+++ rh.pagamento/src/main/java/rh/javapleno/pagamento/service/PagamentoService.java
@@ -1,30 +1,19 @@
 package rh.javapleno.pagamento.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import rh.javapleno.pagamento.component.ColaboradorFeignClient;
 import rh.javapleno.pagamento.model.Colaborador;
 import rh.javapleno.pagamento.model.Pagamento;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Service
 public class PagamentoService {
-
-    @Value("${rh-colaborador.host}")
-    private String pagamentoHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private ColaboradorFeignClient colaboradorFeignClient;
 
     public Pagamento getPagamento(long colaboradorId, int dias) {
 
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", "" + colaboradorId);
-
-        Colaborador colaborador = restTemplate.getForObject(pagamentoHost + "/colaborador/{id}", Colaborador.class, uriVariables);
+        Colaborador colaborador = colaboradorFeignClient.findById((colaboradorId)).getBody();
         return new Pagamento(colaborador.getNome(), colaborador.getValorDia(), dias);
     }
 }
