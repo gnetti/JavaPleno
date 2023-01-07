188efb696793ed20c3d1f95b2bcbc19474bb15a1
Luiz Generoso <luizerytre@gmail.com>
2023-01-07 18:19:22 -0300

    Adicionado Eureka Server v1.0.

---
+++ rh.eureka/src/main/java/rh/javapleno/eureka/RhEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package rh.javapleno.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RhEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhEurekaServerApplication.class, args);
+	}
+
+}
---
+++ rh.eureka/src/test/java/rh/javapleno/eureka/RhEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package rh.javapleno.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
