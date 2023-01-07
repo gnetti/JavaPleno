31b3c31f625ced323866d6bb2d48a78197c7b3ab
Luiz Generoso <luizerytre@gmail.com>
2023-01-07 18:40:51 -0300

    Configuração Eureka Server v1.1.

--- rh.colaborador/src/main/java/rh/javapleno/colaborador/RhColaboradorApplication.java
+++ rh.colaborador/src/main/java/rh/javapleno/colaborador/RhColaboradorApplication.java
@@ -2,7 +2,9 @@ package rh.javapleno.colaborador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class RhColaboradorApplication {
 
--- rh.pagamento/src/main/java/rh/javapleno/pagamento/RhPagamentoApplication.java
+++ rh.pagamento/src/main/java/rh/javapleno/pagamento/RhPagamentoApplication.java
@@ -2,10 +2,10 @@ package rh.javapleno.pagamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "rh-colaborador")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class RhPagamentoApplication {
