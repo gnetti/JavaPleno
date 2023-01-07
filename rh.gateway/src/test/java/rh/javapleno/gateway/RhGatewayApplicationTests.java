017b22655eff9194ab6927dceafb4a4369db8d51
Luiz Generoso <luizerytre@gmail.com>
2023-01-07 20:44:56 -0300

    Adicionado Gateway v1.0

---
+++ rh.gateway/src/test/java/rh/javapleno/gateway/RhGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package rh.javapleno.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
