d2f4d988050b42311c9047d77407948cb13a84f6
Luiz Generoso <luizerytre@gmail.com>
2023-01-07 20:18:23 -0300

    Adicionado Hystrix para tratamento de tolerância e queda de microserviços.

--- rh.pagamento/src/main/java/rh/javapleno/pagamento/controller/PagamentoController.java
+++ rh.pagamento/src/main/java/rh/javapleno/pagamento/controller/PagamentoController.java
@@ -1,5 +1,6 @@
 package rh.javapleno.pagamento.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +17,16 @@ public class PagamentoController {
     @Autowired
     private PagamentoService pagamentoService;
 
+    @HystrixCommand(fallbackMethod = "getPagamentoAlternative")
     @GetMapping(value = "/{colaboradorId}/dias/{dias}")
     public ResponseEntity<Pagamento> getPagamento(@PathVariable Long colaboradorId, @PathVariable Integer dias) {
     Pagamento pagamento = pagamentoService.getPagamento(colaboradorId,dias);
     return ResponseEntity.ok(pagamento);
+
+    }
+    public ResponseEntity<Pagamento> getPagamentoAlternative( Long colaboradorId,  Integer dias) {
+        Pagamento pagamento = new Pagamento("Luiz",400.0, dias);
+        return ResponseEntity.ok(pagamento);
 
     }
 }
