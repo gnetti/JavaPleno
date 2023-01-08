f07273d832fd43fa0d6ff7fe6100b661ac773d83
Luiz Generoso <luizerytre@gmail.com>
2023-01-08 20:12:46 -0300

    Adicionado perfil do usuario v1.0

--- rh.usuario/src/main/java/rh/javapleno/usuario/controller/PerfilController.java
+++ rh.usuario/src/main/java/rh/javapleno/usuario/controller/PerfilController.java
@@ -2,14 +2,48 @@ package rh.javapleno.usuario.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rh.javapleno.usuario.model.Perfil;
+import rh.javapleno.usuario.service.PerfilService;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
-@RefreshScope
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/perfil")
 public class PerfilController {
+    private final PerfilService perfilService;
+
+    @PostMapping
+    public Perfil salvar(@RequestBody Perfil perfil) {
+
+        return perfilService.salvar(perfil);
+    }
+
+    @GetMapping
+    public List<Perfil> PesquisaTodos() {
+        return perfilService.pesquisaTodos();
+    }
+
+    @GetMapping("{id}")
+    public Optional<Perfil> pesquisarId(@PathVariable Long id) {
+
+        return perfilService.pesquisarId(id);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> alterar(@RequestBody Perfil perfil) {
+        perfilService.alterar(perfil);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Long id) {
+        perfilService.deletar(id);
+    }
 }
