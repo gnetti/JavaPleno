1e2384d08bddad03b97087c35808c8f09b9d931f
Luiz Generoso <luizerytre@gmail.com>
2023-01-08 10:25:02 -0300

    Adicionado Server config v1.0.

---
+++ rh.server/src/test/java/rh/javapleno/server/RhServerApplicationTests.java
@@ -0,0 +1,13 @@
+package rh.javapleno.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
