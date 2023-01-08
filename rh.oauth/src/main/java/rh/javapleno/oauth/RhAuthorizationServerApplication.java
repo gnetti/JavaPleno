875333655250799e5279680e5be68bee25566c80
Luiz Generoso <luizerytre@gmail.com>
2023-01-08 20:47:14 -0300

    Adicionado Server Authorization.

---
+++ rh.oauth/src/main/java/rh/javapleno/oauth/RhAuthorizationServerApplication.java
@@ -0,0 +1,15 @@
+package rh.javapleno.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class RhAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ rh.usuario/src/main/java/rh/javapleno/usuario/RhUsuarioApplication.java
@@ -0,0 +1,15 @@
+package rh.javapleno.usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class RhUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhUsuarioApplication.class, args);
+	}
+
+}
