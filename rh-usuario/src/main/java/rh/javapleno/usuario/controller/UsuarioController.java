7cedac630280197ad64d4c047f9c00bfbc99f382
Danilo Azevedo <danilo.ann@gmail.com>
2023-02-11 15:54:35 -0300

    Ajuste Nomeclatura de Metodo

--- rh-usuario/src/main/java/rh/javapleno/usuario/controller/UsuarioController.java
+++ rh-usuario/src/main/java/rh/javapleno/usuario/controller/UsuarioController.java
@@ -52,7 +52,7 @@ public class UsuarioController {
 
 
     @GetMapping
-    public List<Usuario> PesquisaTodos() {
+    public List<Usuario> pesquisaTodos() {
         return usuarioService.pesquisaTodos();
     }
 
