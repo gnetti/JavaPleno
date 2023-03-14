d6ab8b9c693e28bc10d6e9f963b1559b1cc271f9
Luiz Generoso <luizerytre@gmail.com>
2023-03-14 19:22:06 -0300

    adicionado busca lancamento em colaborador.

--- rh-usuario/src/main/java/rh/javapleno/usuario/controller/UsuarioController.java
+++ rh-usuario/src/main/java/rh/javapleno/usuario/controller/UsuarioController.java
@@ -83,10 +83,6 @@ public class UsuarioController {
     public UsuarioDTO pesquisarIdColaborador(@PathVariable Long id) {
         return new ModelMapper().map(usuarioService.pesquisarIdColaborador(id), UsuarioDTO.class);
     }
-    @GetMapping(value = "/colaborador/email")
-    public UsuarioDTO pesquisarIdColaborador(@RequestParam String email) {
-        return new ModelMapper().map(usuarioService.pesquisarEmailColaborador(email), UsuarioDTO.class);
-    }
 
     @GetMapping(value = "/colaborador/email")
     public UsuarioDTO pesquisarIdColaborador(@RequestParam String email) {
